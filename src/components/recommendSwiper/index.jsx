7dd09c6aaaf88c4dcc8baa85fb1c627e3c850ede
李强进 <li.qj@ikcrm.com>
2019-08-13 17:39:30 +0800

    修改

--- src/components/recommendSwiper/index.jsx
+++ src/components/recommendSwiper/index.jsx
@@ -1,32 +1,34 @@
 import Taro from '@tarojs/taro'
-import { View, Swiper, SwiperItem, Image } from '@tarojs/components'
-import fly from '../../assets/fly.jpeg'
+import {Image, Swiper, SwiperItem, View} from '@tarojs/components'
+import qinghua from '../../assets/qinghua.jpg'
+import weiminhu from '../../assets/weiminhu.jpg'
+import wuhandaxue from '../../assets/wuhandaxue.jpg'
 import './index.scss'
 
 /** 发现页顶部的推荐图片 */
 export class RecommendSwiper extends Taro.Component {
-  state = {
-    data: [fly],
-  }
+    state = {
+        data: [qinghua, weiminhu, wuhandaxue],
+    }
 
-  render() {
-    const { data } = this.state
-    return (
-      <View>
-        <Swiper
-          indicatorColor="#999"
-          indicatorActiveColor="#888"
-          circular
-          indicatorDots
-          autoplay
-        >
-          {data.map((image, index) => (
-            <SwiperItem key={index}>
-              <Image src={image} class="slide-image" mode="aspectFill" />
-            </SwiperItem>
-          ))}
-        </Swiper>
-      </View>
-    )
-  }
+    render() {
+        const {data} = this.state
+        return (
+            <View>
+                <Swiper
+                    indicatorColor="#999"
+                    indicatorActiveColor="#888"
+                    circular
+                    indicatorDots
+                    autoplay
+                >
+                    {data.map((image, index) => (
+                        <SwiperItem key={index}>
+                            <Image src={image} class="slide-image" mode="aspectFill"/>
+                        </SwiperItem>
+                    ))}
+                </Swiper>
+            </View>
+        )
+    }
 }
